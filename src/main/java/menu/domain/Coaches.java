0a5f7c2322fb36083da26f90fad5b6b00240ee30
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 13:46:31 +0900

    feat: 코치 인원 수 검증 로직 작성

---
+++ src/main/java/menu/domain/Coaches.java
@@ -0,0 +1,22 @@
+package menu.domain;
+
+import java.util.List;
+import menu.common.ErrorMessage;
+
+public class Coaches {
+
+    private static final int MAX_COACHES = 5;
+    private static final int MIN_COACHES = 2;
+
+    private final List<Coach> coaches;
+
+    public Coaches(final List<Coach> coaches) {
+        this.coaches = coaches;
+    }
+
+    private void validate(final List<Coach> coaches) {
+        if (coaches.size() < MIN_COACHES || coaches.size() > MAX_COACHES) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_COACH_SIZE.getMessage());
+        }
+    }
+}
