50ac9c284f2ca1423a1272469541bce016e0975c
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 13:09:30 +0900

    feat: 코치 이름 입력 받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,20 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import menu.common.ErrorMessage;
+
 public class InputView {
+
+    public String inputCoachNames() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        String input = Console.readLine();
+        validate(input);
+        return input;
+    }
+
+    private void validate(final String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getMessage());
+        }
+    }
 }
