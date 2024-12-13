2b7b0ee343d805c0a965a82682441fa9dfd70e82
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 13:44:12 +0900

    feat: 코치 객체 생성 및 이름 검증

--- src/main/java/menu/common/ErrorMessage.java
+++ src/main/java/menu/common/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
 
     INVALID_INPUT("아무것도 입력되지 않았습니다. 다시 입력해주세요."),
     INVALID_INPUT_TYPE("숫자로 입력해주세요."),
-    NOT_FOUND_CATEGORY("카테고리를 찾을 수 없습니다.");
+    NOT_FOUND_CATEGORY("카테고리를 찾을 수 없습니다."),
+    INVALID_NAME_LENGTH("코치 이름은 최소 2글자에서 최대 4글자이여야 합니다.");
 
     private static final String HEADER = "[ERROR] ";
     private final String message;
