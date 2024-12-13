e59e505c57de2df24e182ae7708b4863eee6449c
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 14:08:58 +0900

    feat: 입력 문자열에서 메뉴 이름 파싱 로직 작성

--- src/main/java/menu/domain/utils/DataParser.java
+++ src/main/java/menu/domain/utils/DataParser.java
@@ -3,6 +3,8 @@ package menu.domain.utils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import menu.common.ErrorMessage;
+import menu.domain.Menu;
 import menu.domain.coach.Coach;
 
 public class DataParser {
@@ -13,5 +15,17 @@ public class DataParser {
         return Arrays.stream(names.split(DELIMITER))
                 .map(name -> new Coach(name.trim()))
                 .collect(Collectors.toList());
+    }
+
+    public static List<String> parseMenuName(final String menus) {
+        return Arrays.stream(menus.split(DELIMITER))
+                .peek(DataParser::validateMenuName)
+                .collect(Collectors.toList());
+    }
+
+    private static void validateMenuName(final String menuName) {
+        if (!Menu.isExist(menuName)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_MENU_NAME.getMessage());
+        }
     }
 }
