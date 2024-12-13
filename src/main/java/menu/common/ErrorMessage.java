fa83fd577ef062a3a2c6f6d985ab8f3b9d33a868
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 13:05:56 +0900

    feat: 에러 메세지 출력

---
+++ src/main/java/menu/common/ErrorMessage.java
@@ -0,0 +1,18 @@
+package menu.common;
+
+public enum ErrorMessage {
+
+    INVALID_INPUT("아무것도 입력되지 않았습니다. 다시 입력해주세요."),
+    INVALID_INPUT_TYPE("숫자로 입력해주세요.");
+
+    private static final String HEADER = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return HEADER + message;
+    }
+}
---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,8 @@
+package menu.view;
+
+public class OutputView {
+
+    public void printErrorMessage(final String message) {
+        System.out.println(message);
+    }
+}
