9bbcdb33fe9e151ac66fd4f15fddea9a29e65d54
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 13:07:42 +0900

    feat: 시작 메세지 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,6 +2,12 @@ package menu.view;
 
 public class OutputView {
 
+    private static final String NEW_LINE = System.lineSeparator();
+
+    public void printWelcomeMessage() {
+        System.out.println("점심 메뉴 추천을 시작합니다." + NEW_LINE);
+    }
+
     public void printErrorMessage(final String message) {
         System.out.println(message);
     }
