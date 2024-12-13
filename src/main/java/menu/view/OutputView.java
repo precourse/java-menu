6a5a84f1dd45ba7c20022b7e1d4d13d9c0b287ca
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 13:10:18 +0900

    feat: 종료 메세지 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -8,6 +8,10 @@ public class OutputView {
         System.out.println("점심 메뉴 추천을 시작합니다." + NEW_LINE);
     }
 
+    public void printEndMessage() {
+        System.out.println(NEW_LINE + "추천을 완료했습니다.");
+    }
+
     public void printErrorMessage(final String message) {
         System.out.println(message);
     }
