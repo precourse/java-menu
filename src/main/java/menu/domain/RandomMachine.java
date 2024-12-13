92a292c8a26d6cb88c7f6a8e2167bb85a3e0653f
kgy1008 <gayeon1008@g.skku.edu>
2024-12-13 14:25:13 +0900

    feat: 카테고리 랜덤 생성

--- src/main/java/menu/domain/RandomMachine.java
+++ src/main/java/menu/domain/RandomMachine.java
@@ -1,4 +1,14 @@
 package menu.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomMachine {
+
+    private static final int MAX_NUMBER = 5;
+    private static final int MIN_NUMBER = 1;
+
+    public Category pickRandomCategory() {
+        int number = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+        return Category.findCategory(number);
+    }
 }
--- src/main/java/menu/domain/Recommender.java
+++ src/main/java/menu/domain/Recommender.java
@@ -1,10 +1,21 @@
 package menu.domain;
 
+import menu.domain.coach.Coaches;
+
 public class Recommender {
+
+    private static final int TOTAL_DAY = 5;
 
     private final RandomMachine randomMachine;
 
     public Recommender(final RandomMachine randomMachine) {
         this.randomMachine = randomMachine;
+    }
+
+
+    public void recommend(final Coaches coaches) {
+        for (int i = 0; i < TOTAL_DAY; i++) {
+            Category category = randomMachine.pickRandomCategory();
+        }
     }
 }
